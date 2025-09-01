8979071567b4612d43cfcfc342db64ccb86be69a
Vineet Mishra <mishra.v89@gmail.com>
2025-09-01 13:41:27 +0530

    Added fix

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -1,29 +1,29 @@
-create table if not exists courses (
-  id uuid primary key,
-  title varchar(255) not null,
-  category varchar(100),
-  instructor_name varchar(255),
-  duration_minutes integer,
-  thumbnail varchar(500),
-  created_at timestamp,
-  updated_at timestamp
+CREATE TABLE IF NOT EXISTS onedrive_db.training_details
+(
+    training_detail_id character varying(4000) COLLATE pg_catalog."default" NOT NULL,
+    training_id character varying(255) COLLATE pg_catalog."default",
+    category character varying(255) COLLATE pg_catalog."default",
+    module_duration integer,
+    module_name character varying(4000) COLLATE pg_catalog."default",
+    module_path character varying(255) COLLATE pg_catalog."default",
+    module_topic character varying(4000) COLLATE pg_catalog."default",
+    training_topic character varying(4000) COLLATE pg_catalog."default",
+    instructor_name character varying(4000) COLLATE pg_catalog."default",
+    CONSTRAINT training_details_pkey PRIMARY KEY (training_detail_id)
+)
+
+
+DROP TABLE IF EXISTS onedrive_db.enrollments;
+	CREATE TABLE IF NOT EXISTS onedrive_db.enrollments (
+    id BIGSERIAL PRIMARY KEY,  -- auto-generated number
+    user_id VARCHAR(255) NOT NULL,
+    course_id VARCHAR(4000) NOT NULL REFERENCES onedrive_db.training_details(training_detail_id),
+    progress_percent INTEGER NOT NULL DEFAULT 0,
+    last_accessed_at TIMESTAMP,
+    status VARCHAR(50) NOT NULL
 );
 
-create table if not exists course_topics (
-  course_id uuid references courses(id) on delete cascade,
-  topic varchar(100) not null
-);
-
-create table if not exists enrollments (
-  id varchar(255) primary key,
-  user_id varchar(255) not null,
-  course_id text not null references courses(id),
-  progress_percent integer not null default 0,
-  last_accessed_at timestamp,
-  status varchar(50) not null
-);
-
-create table if not exists certificates (
+create table if not exists onedrive_db.certificates (
   id varchar(255) primary key,
   user_id varchar(255) not null,
   course_id VARCHAR(4000) NOT NULL REFERENCES onedrive_db.training_details(training_detail_id),
