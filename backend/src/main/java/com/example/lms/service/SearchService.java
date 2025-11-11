c1d35336a56b8a2a32cf6b5162994a6f7a1c773a
Rajib Bhattacharya <rajib.bhattacharya@irissoftware.com>
2025-11-11 09:28:40 +0530

    Fix in getTrainingList in Search

--- backend/src/main/java/com/example/lms/service/SearchService.java
+++ backend/src/main/java/com/example/lms/service/SearchService.java
@@ -138,7 +138,7 @@ public class SearchService {
         log.info("got results");
         return results.stream()
           .map(row -> new TrainingNameDto(
-            (String)row[0],(String)row[1],(Integer)row[2]
+            (String)row[0],(String)row[1],(Long)row[2]
           ))
           .collect(Collectors.toList());
       } else {
